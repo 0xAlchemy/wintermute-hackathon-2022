4c2843aa467c2eaafefab5bcaa929d50f0f6c310
ikachko <ilyakachko@gmail.com>
2022-09-11 16:35:53 +0300

    added status and submitx

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from quart import Quart, request
+from hexbytes import HexBytes
 
 class AuctionHouse:
     def __init__(self) -> None:
         pass
 
-    async def get_tx_pool(self, pubKey: str) -> dict:
+    async def get_tx_pool(self, pubKey: HexBytes) -> dict:
         return {"txPool": []}
 
-    async def submit_bid(self, pubKey: str, txHash: str, value: int) -> dict:
+    async def submit_bid(self, pubKey: HexBytes, txHash: HexBytes, value: int) -> dict:
         return {"status": True}
 
-    async def results(self, pubKey: str) -> dict:
+    async def results(self, pubKey: HexBytes) -> dict:
         return {"results": []}
     
-    async def register(self, pubKey: str) -> dict:
+    async def register(self, pubKey: HexBytes) -> dict:
+        return {"status": True}
+    
+    async def get_status(self, pubkey: HexBytes) -> dict:
+        return {"status": True}
+    
+    async def submit_tx(self, tx_raw: HexBytes) -> dict:
         return {"status": True}
 
 
@@ -26,7 +33,7 @@ def register_routes(app: Quart, ah: AuctionHouse):
 
         data = await request.get_json()
         assert("pubKey" in data)
-        pubKey = data["pubKey"]
+        pubKey = HexBytes(data["pubKey"])
 
         res = await ah.get_tx_pool(pubKey)
 
@@ -43,10 +50,12 @@ def register_routes(app: Quart, ah: AuctionHouse):
 
         assert(
             "pubKey" in data
-            and "txHash" in data    
+            and "txHash" in data
             and "value" in data
         )
-        pubKey, txHash, value = data["pubKey"], data["txHash"], data["value"]
+        pubKey, txHash, value = (
+            HexBytes(data["pubKey"]), HexBytes(data["txHash"]), int(data["value"])
+        )
 
         try:
             res = await ah.submit_bid(pubKey, txHash, value)
@@ -63,7 +72,7 @@ def register_routes(app: Quart, ah: AuctionHouse):
 
         data = await request.get_json()
         assert("pubKey" in data)
-        pubKey = data["pubKey"]
+        pubKey = HexBytes(data["pubKey"])
 
         res = await ah.results(pubKey)
 
@@ -77,9 +86,37 @@ def register_routes(app: Quart, ah: AuctionHouse):
 
         data = await request.get_json()
         assert("pubKey" in data)
-        pubKey = data["pubKey"]
+        pubKey = HexBytes(data["pubKey"])
 
         res = await ah.register(pubKey)
+
+        return res
+    
+    @app.route("/submitTx", methods=["POST"])
+    async def submit_tx():
+        """
+        input: {"txRaw": "0xabcdef.."}
+        """
+
+        data = await request.get_json()
+        assert("txRaw" in data)
+        tx_raw = HexBytes(data["txRaw"])
+
+        res = await ah.submit_tx(tx_raw)
+
+        return res
+
+    @app.route("/getStatus", methods=["GET"])
+    async def get_status() -> dict:
+        """
+        input: {"pubKey": 0x...}
+        """
+
+        data = await request.get_json()
+        assert("pubKey" in data)
+        pubKey = HexBytes(data["pubKey"])
+
+        res = await ah.get_status(pubKey)
 
         return res
 
